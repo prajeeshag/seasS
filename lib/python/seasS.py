30595314ef44511a89c2384ae3f147ce1367fab3
Prajeesh Ag <prajeeshag@gmail.com>
2025-01-21 15:20:50 +0300

    Added rainfall

--- lib/python/seasS.py
+++ lib/python/seasS.py
@@ -20,11 +20,11 @@ MODELS = [
 PERCENTILES = [10, 33, 66, 90]
 
 
-def get_url(model: str, year: int, mon_abbr: str, lstart: int, lend: int):
+def get_url(model: str, year: int, mon_abbr: str, lstart: int, lend: int, fldname: str):
     lead_range = f"L/{lstart}.5/{lend}.5/RANGEEDGES/"
     url1 = f"http://iridl.ldeo.columbia.edu/SOURCES/.Models/.NMME/.{model}/"
     area = "Y/5/40/RANGE/X/20/60/RANGEEDGES"
-    url2 = f".MONTHLY/.tref/{area}/S/%28%201%20{mon_abbr}%20{year}%29/VALUES/{lead_range}%5BL%5D/average/data.nc"
+    url2 = f".MONTHLY/.{fldname}/{area}/S/%28%201%20{mon_abbr}%20{year}%29/VALUES/{lead_range}%5BL%5D/average/data.nc"
     if model == "COLA-RSMAS-CESM1":
         return f"{url1}{url2}"
     elif model == "NCEP-CFSv2":
@@ -49,14 +49,16 @@ def download_data(url: str, output_path: str) -> None:
     shutil.move(temp_file, output_path)
 
 
-def get_hist_pctl_path(model: str, month: int, lstart: int, lend: int, root_path: str):
-    return f"{root_path}/hist/{model}/percentiles/{month:02d}/L-{lstart}.5-{lend}.5/{model}-{month:02d}-L-{lstart}.5-{lend}.5_percentiles.nc"
+def get_hist_pctl_path(
+    model: str, month: int, lstart: int, lend: int, root_path: str, fldname: str
+):
+    return f"{root_path}/{fldname}/hist/{model}/percentiles/{month:02d}/L-{lstart}.5-{lend}.5/{model}-{month:02d}-L-{lstart}.5-{lend}.5_percentiles.nc"
 
 
-def compute_frequencies(data_file, hist_pctl_data):
+def compute_frequencies(data_file, hist_pctl_data, fldname):
     ds = xr.open_dataset(data_file, decode_times=False)
     hist_ds = xr.open_dataset(hist_pctl_data)
-    var = ds["tref"].squeeze()
+    var = ds[fldname].squeeze()
     if len(var.shape) > 3 or len(var.shape) < 2:
         raise SystemError(f"var.shapre {var.shape}")
     ne = 1
@@ -79,51 +81,65 @@ def compute_frequencies(data_file, hist_pctl_data):
     return ne, res
 
 
-def get_fcst_file_path(year, month, lstart, lend, model, root_path):
-    data_file = f"{root_path}/fcst/{model}/averages/{year}/{month:02d}/L-{lstart}.5-{lend}.5/data.nc"
+def get_fcst_file_path(year, month, lstart, lend, model, root_path, fldname):
+    data_file = f"{root_path}/{fldname}/fcst/{model}/averages/{year}/{month:02d}/L-{lstart}.5-{lend}.5/data.nc"
     Path(data_file).parent.mkdir(
         parents=True, exist_ok=True
     )  # Create directories if they don't exist
     return data_file
 
 
-def get_mme_freq_path(year: int, month: int, lstart: int, lend: int, data_root: str):
-    return f"{data_root}/MME/frequencies/{year}/{month:02d}/L-{lstart}.5-{lend}.5/MME-L-{lstart}.5-{lend}.5_frequencies.nc"
+def get_mme_freq_path(
+    year: int, month: int, lstart: int, lend: int, data_root: str, fldname: str
+):
+    return f"{data_root}/{fldname}/MME/frequencies/{year}/{month:02d}/L-{lstart}.5-{lend}.5/MME_{fldname}-L-{lstart}.5-{lend}.5_frequencies.nc"
 
 
 @app.command()
-def get_fcst_data(year: int, month: int, lstart: int, lend: int, data_root: str):
+def get_fcst_data(
+    year: int, month: int, lstart: int, lend: int, data_root: str, fldname: str
+):
     mon_abbr = calendar.month_abbr[month]
     for model in MODELS:
-        url = get_url(model, year, mon_abbr, lstart, lend)
-        data_file = get_fcst_file_path(year, month, lstart, lend, model, data_root)
+        url = get_url(model, year, mon_abbr, lstart, lend, fldname)
+        data_file = get_fcst_file_path(
+            year, month, lstart, lend, model, data_root, fldname
+        )
         print(f"downloading {data_file}")
         download_data(url, data_file)
 
 
 @app.command()
-def process_mme_freq(year: int, month: int, lstart: int, lend: int, data_root: str):
+def process_mme_freq(
+    year: int, month: int, lstart: int, lend: int, data_root: str, fldname: str
+):
     for n, model in enumerate(MODELS):
-        data_file = get_fcst_file_path(year, month, lstart, lend, model, data_root)
-        hist_pctl_data = get_hist_pctl_path(model, month, lstart, lend, data_root)
+        data_file = get_fcst_file_path(
+            year, month, lstart, lend, model, data_root, fldname
+        )
+        hist_pctl_data = get_hist_pctl_path(
+            model, month, lstart, lend, data_root, fldname
+        )
         if n == 0:
-            ne, fr = compute_frequencies(data_file, hist_pctl_data)
+            ne, fr = compute_frequencies(data_file, hist_pctl_data, fldname)
         else:
-            nne, ffr = compute_frequencies(data_file, hist_pctl_data)
+            nne, ffr = compute_frequencies(data_file, hist_pctl_data, fldname)
             ne += nne
             for i in range(len(fr)):
                 fr[i].values += ffr[i].values
     for i in range(len(fr)):
         fr[i].values = fr[i].values / float(ne)
     dataset = xr.Dataset({da.name: da for da in fr})
-    output = get_mme_freq_path(year, month, lstart, lend, data_root)
+    output = get_mme_freq_path(year, month, lstart, lend, data_root, fldname)
     Path(output).parent.mkdir(parents=True, exist_ok=True)
     dataset.to_netcdf(output)
 
 
 @app.command()
-def mme_freq_path(year: int, month: int, lstart: int, lend: int, data_root: str):
-    print(get_mme_freq_path(year, month, lstart, lend, data_root))
+def mme_freq_path(
+    year: int, month: int, lstart: int, lend: int, data_root: str, fldname: str
+):
+    print(get_mme_freq_path(year, month, lstart, lend, data_root, fldname))
 
 
 if __name__ == "__main__":
